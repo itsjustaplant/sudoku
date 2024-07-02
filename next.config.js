1163e990b2c694465c755f87db4412b2910e41f0
itsjustaplant <alperensahin442@gmail.com>
2024-07-02 20:15:19 +0000

    fix :: remove redundant action config and asset prefix

--- next.config.js
+++ next.config.js
@@ -2,14 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
-  assetPrefix: process.env.NODE_ENV === "development" ? '' : "/sudoku/",
 
   experimental: {
     serverActions: {
-      allowedForwardedHosts: ["http://localhost", "https://justaplant.dev", "http://localhost:3005", "https://justaplant.dev/sudoku", "https://justaplant.dev/", "justaplant.dev"],
-      allowedOrigins: ["http://localhost", "https://justaplant.dev", "http://localhost:3005", "https://justaplant.dev/sudoku", "https://justaplant.dev/", "justaplant.dev"]
+      allowedForwardedHosts: ['http://localhost', 'https://justaplant.dev', 'http://localhost:3005', 'https://justaplant.dev/sudoku', 'https://justaplant.dev/', 'justaplant.dev'],
+      allowedOrigins: ['http://localhost', 'https://justaplant.dev', 'http://localhost:3005', 'https://justaplant.dev/sudoku', 'https://justaplant.dev/', 'justaplant.dev']
     }
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
